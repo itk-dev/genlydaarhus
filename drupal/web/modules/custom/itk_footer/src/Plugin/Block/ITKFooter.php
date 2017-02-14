988156dcdb92e1a25b55a74c5cdc537e977e8b67
Troels Ugilt Jensen <tujensen@gmail.com>
2017-02-14 13:23:35 +0100

    Issue #GEN-57: Removed itkore modules. Moved to separate modules

---
+++ drupal/web/modules/custom/itk_footer/src/Plugin/Block/ITKFooter.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Drupal\itk_footer\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Menu\MenuTreeParameters;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Provides footer content
+ *
+ * @Block(
+ *   id = "itk_footer",
+ *   admin_label = @Translation("ITK footer"),
+ * )
+ */
+class ITKFooter extends BlockBase {
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $config = \Drupal::getContainer()->get('itk_footer.config')->getAll();
+
+    $footer_text = check_markup($config['footer_text'], 'filtered_html');
+
+    return array(
+      '#type' => 'markup',
+      '#theme' => 'itk_footer_block',
+      '#cache' => array(
+        'max-age' => 0,
+      ),
+      '#footer_text' => $footer_text,
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+    $form = parent::blockForm($form, $form_state);
+
+    return $form;
+  }
+}
+?>
