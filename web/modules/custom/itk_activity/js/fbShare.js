23e2a02a25234d77eafb6060de37b9da9e508b14
Troels Ugilt Jensen <tujensen@gmail.com>
2017-03-14 09:33:32 +0100

    Issue #GEN-86: Fixed documentation

--- web/modules/custom/itk_activity/js/fbShare.js
+++ web/modules/custom/itk_activity/js/fbShare.js
@@ -1,10 +1,11 @@
 /**
  * @file
- * Contains javascript to get field_area from field_zipcode using dawa.
+ * Contains javascript to enable facebook share button.
  */
 
 (function ($) {
   $(document).ready(function () {
+    // Initialize facebook.
     window.fbAsyncInit = function() {
       FB.init({
         appId      : drupalSettings.fbShare.appId,
@@ -13,7 +14,6 @@
       });
       FB.AppEvents.logPageView();
     };
-
     (function(d, s, id){
       var js, fjs = d.getElementsByTagName(s)[0];
       if (d.getElementById(id)) {return;}
@@ -22,6 +22,7 @@
       fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));
 
+    // Setup click listener for facebook button.
     $('.js-facebook-share-button').click(function () {
       FB.ui({
         method: 'share',
