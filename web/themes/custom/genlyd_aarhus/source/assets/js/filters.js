1517f31554482fd9b432423710238d9d0dddc1aa
Troels Ugilt Jensen <tujensen@gmail.com>
2017-03-17 14:12:34 +0100

    Issue #GEN-137: Started on filters for activities page

--- web/themes/custom/genlyd_aarhus/source/assets/js/filters.js
+++ web/themes/custom/genlyd_aarhus/source/assets/js/filters.js
@@ -1,11 +1,17 @@
-jQuery(document).ready(function($) {
+/**
+ * @file
+ * Supplies functionality for filters button.
+ */
 
+jQuery(document).ready(function ($) {
   // Find button and filters in DOM
   var button = $('.js-filters-toggle');
   var filters = $('.js-filters');
 
   // Show and hide filters on click
-  button.click(function(){
+  button.click(function click(event) {
+    event.preventDefault();
+    event.stopPropagation();
     filters.toggle();
   });
 });
