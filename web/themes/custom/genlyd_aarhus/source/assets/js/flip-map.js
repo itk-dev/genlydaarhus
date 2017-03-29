03dbc32d842e71d86aeaae7a308fc7ed57977f6c
Troels Ugilt Jensen <tujensen@gmail.com>
2017-03-29 10:28:50 +0200

    Issue #GEN-195: Fixed js for buttons

--- web/themes/custom/genlyd_aarhus/source/assets/js/flip-map.js
+++ web/themes/custom/genlyd_aarhus/source/assets/js/flip-map.js
@@ -2,6 +2,8 @@ jQuery(document).ready(function($) {
   var mapTab = $('.js-map-tab-map');
   var viewTab = $('.js-map-tab-view');
   var locationBtn = $('.js-maps-my-location');
+  var filterBtn = $('.js-filters-toggle');
+  var filters = $('.js-all-filters');
   var btn = $('.js-maps-switch');
 
   mapTab.hide();
@@ -12,16 +14,20 @@ jQuery(document).ready(function($) {
     event.stopPropagation();
 
     if (!mapTab.is(':visible')) {
-      btn.text(Drupal.t('Show table'));
+      btn.text('Show table');
+      filterBtn.hide();
+      filters.hide();
       viewTab.hide();
       mapTab.show();
       locationBtn.show();
     }
     else {
-      btn.text(Drupal.t('Show map'));
+      btn.text('Show map');
+      filterBtn.show();
+      filters.show();
       viewTab.show();
       mapTab.hide();
       locationBtn.hide();
     }
   });
-})(jQuery, Drupal);
+});
