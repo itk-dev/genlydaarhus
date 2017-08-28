1dd2e366a458bff959a3b08e836454599653fc17
Troels Ugilt Jensen <tujensen@gmail.com>
2017-08-28 12:53:29 +0200

    Issue #GEN-354: Fixed upload file indication

--- web/themes/custom/genlyd_aarhus/source/assets/js/image-upload.js
+++ web/themes/custom/genlyd_aarhus/source/assets/js/image-upload.js
@@ -3,33 +3,47 @@
 	Available for use under the MIT License
 
   https://tympanus.net/codrops/2015/09/15/styling-customizing-file-inputs-smart-way/
+
+  NB! Modified.
 */
 
 jQuery(document).ready(function ($) {
   "use strict";
 
-	$('.js-image-upload').each( function() {
-		var $input	 = $( this ),
-			$label	 = $input.next( 'label' ),
-			labelVal = $label.html();
+  $('.js-image-upload').each(function () {
+    var $input = $(this),
+      $label = $input.next('label'),
+      infoText = $('#js-upload-info'),
+      labelVal = $label.html(),
+      fileChosenText = Drupal.t('You have chosen the file:'),
+      chooseAnotherText = Drupal.t('Choose another');
 
-		$input.on('change', function(e) {
-			var fileName = '';
+    $input.on('change', function (e) {
+      var fileName = '';
 
-      if (e.target.value)
-				fileName = e.target.value.split( '\\' ).pop();
+      if (e.target.value) {
+        fileName = e.target.value.split('\\').pop();
+      }
 
-			if (fileName) {
-        $label.find('span').html(fileName);
-      } else {
+      if (fileName) {
+        infoText.html(fileChosenText + ' ' + fileName);
+        $label.find('span').html(chooseAnotherText);
+        $label.removeClass("is-cta");
+        $label.addClass('is-secondary-light');
+      }
+      else {
         $label.html(labelVal);
       }
-		});
+    });
 
-		// Firefox bug fix.
-		$input
-		.on( 'focus', function() { $input.addClass( 'has-focus' ); } )
-		.on( 'blur', function() { $input.removeClass( 'has-focus' ); } );
-	});
+    // Firefox bug fix.
+    $input
+    .on('focus', function () {
+      $input.addClass('has-focus');
+    })
+    .on('blur', function () {
+      $input.removeClass('has-focus');
+    });
+  });
 
 });
